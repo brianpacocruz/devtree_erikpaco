26cad5e000e1e7e8eb63c3851c5bac19018f832f
camila medina <medinacamila714@gmail.com>
2026-05-06 11:57:47 -0300

    hola

---
+++ src/views/LoginView.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function LoginView() {
+  return (
+    <>
+        <div>LoginView</div>
+        <nav>
+            <Link to="/auth/register"> 
+                ¿No tienes una cuenta? Crea una aqui
+            </Link>
+        </nav>
+    </>
+
+  )
+}
